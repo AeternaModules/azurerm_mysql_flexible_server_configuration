af54c7c49bc500b8c9e7eec703356f4e5dab09b7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "mysql_flexible_server_configurations" {
-  description = "All mysql_flexible_server_configuration resources"
-  value       = azurerm_mysql_flexible_server_configuration.mysql_flexible_server_configurations
-}
 output "mysql_flexible_server_configurations_name" {
-  description = "List of name values across all mysql_flexible_server_configurations"
-  value       = [for k, v in azurerm_mysql_flexible_server_configuration.mysql_flexible_server_configurations : v.name]
+  description = "Map of name values across all mysql_flexible_server_configurations, keyed the same as var.mysql_flexible_server_configurations"
+  value       = { for k, v in azurerm_mysql_flexible_server_configuration.mysql_flexible_server_configurations : k => v.name }
 }
 output "mysql_flexible_server_configurations_resource_group_name" {
-  description = "List of resource_group_name values across all mysql_flexible_server_configurations"
-  value       = [for k, v in azurerm_mysql_flexible_server_configuration.mysql_flexible_server_configurations : v.resource_group_name]
+  description = "Map of resource_group_name values across all mysql_flexible_server_configurations, keyed the same as var.mysql_flexible_server_configurations"
+  value       = { for k, v in azurerm_mysql_flexible_server_configuration.mysql_flexible_server_configurations : k => v.resource_group_name }
 }
 output "mysql_flexible_server_configurations_server_name" {
-  description = "List of server_name values across all mysql_flexible_server_configurations"
-  value       = [for k, v in azurerm_mysql_flexible_server_configuration.mysql_flexible_server_configurations : v.server_name]
+  description = "Map of server_name values across all mysql_flexible_server_configurations, keyed the same as var.mysql_flexible_server_configurations"
+  value       = { for k, v in azurerm_mysql_flexible_server_configuration.mysql_flexible_server_configurations : k => v.server_name }
 }
 output "mysql_flexible_server_configurations_value" {
-  description = "List of value values across all mysql_flexible_server_configurations"
-  value       = [for k, v in azurerm_mysql_flexible_server_configuration.mysql_flexible_server_configurations : v.value]
+  description = "Map of value values across all mysql_flexible_server_configurations, keyed the same as var.mysql_flexible_server_configurations"
+  value       = { for k, v in azurerm_mysql_flexible_server_configuration.mysql_flexible_server_configurations : k => v.value }
 }
 
